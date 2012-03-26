ac5b5e1a68cc0257d7c5b8ff83cd6cd4cc91d358
mattijs <mattijs>
2012-03-26 09:58:32 +0100

    ENH: surfaceFeatureExtract: duplicate feature

--- applications/utilities/surface/surfaceFeatureExtract/surfaceFeatureExtract.C
+++ applications/utilities/surface/surfaceFeatureExtract/surfaceFeatureExtract.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 2011 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2011-2012 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -259,11 +259,6 @@ int main(int argc, char *argv[])
         "featureProximity",
         "scalar",
         "distance to look for close features"
-    );
-    argList::addBoolOption
-    (
-        "writeVTK",
-        "write surface property VTK files"
     );
     argList::addBoolOption
     (
