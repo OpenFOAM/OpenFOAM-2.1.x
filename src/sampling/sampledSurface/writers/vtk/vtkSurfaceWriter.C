641f943b5cea81c8cf41325b82230306fd00c005
mattijs <mattijs>
2012-07-26 16:53:58 +0100

    STYLE: vtkSurfaceWriter: 80 char limit

--- src/sampling/sampledSurface/writers/vtk/vtkSurfaceWriter.C
+++ src/sampling/sampledSurface/writers/vtk/vtkSurfaceWriter.C
@@ -169,7 +169,8 @@ namespace Foam
         forAll(values, elemI)
         {
             const symmTensor& v = values[elemI];
-            os  << float(v[0]) << ' ' << float(v[1]) << ' ' << float(v[2]) << ' '
+            os  << float(v[0]) << ' ' << float(v[1]) << ' ' << float(v[2])
+                << ' '
                 << float(v[3]) << ' ' << float(v[4]) << ' ' << float(v[5])
                 << nl;
 
@@ -189,8 +190,10 @@ namespace Foam
         forAll(values, elemI)
         {
             const tensor& v = values[elemI];
-            os  << float(v[0]) << ' ' << float(v[1]) << ' ' << float(v[2]) << ' '
-                << float(v[3]) << ' ' << float(v[4]) << ' ' << float(v[5]) << ' '
+            os  << float(v[0]) << ' ' << float(v[1]) << ' ' << float(v[2])
+                << ' '
+                << float(v[3]) << ' ' << float(v[4]) << ' ' << float(v[5])
+                << ' '
                 << float(v[6]) << ' ' << float(v[7]) << ' ' << float(v[8])
                 << nl;
         }
