4fc9d5509e9fba8af23263a72dffd983220a9d19
Henry <Henry>
2012-09-07 10:54:43 +0100

    extrudeMesh/extrudedMesh: Updated headers

--- applications/utilities/mesh/generation/extrude/extrudeMesh/extrudedMesh/extrudedMesh.C
+++ applications/utilities/mesh/generation/extrude/extrudeMesh/extrudedMesh/extrudedMesh.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 2011 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2011-2012 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -24,10 +24,6 @@ License
 \*---------------------------------------------------------------------------*/
 
 #include "extrudedMesh.H"
-#include "wallPolyPatch.H"
-#include "meshTools.H"
-#include "ListOps.H"
-#include "OFstream.H"
 
 // * * * * * * * * * * * * * Private Member Functions  * * * * * * * * * * * //
 
