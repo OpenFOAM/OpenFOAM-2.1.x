4a4c06f816a316a45de5790553b3dca463a4cf80
mattijs <mattijs>
2012-12-13 12:18:12 +0000

    BUG: mapped bc: use distribute in mappedPatchBase so AMI is supported

--- src/thermophysicalModels/radiationModels/derivedFvPatchFields/radiationCoupledBase/radiationCoupledBase.C
+++ src/thermophysicalModels/radiationModels/derivedFvPatchFields/radiationCoupledBase/radiationCoupledBase.C
@@ -142,9 +142,6 @@ Foam::scalarField Foam::radiationCoupledBase::emissivity() const
 
             const polyMesh& nbrMesh = mpp.sampleMesh();
 
-            // Force recalculation of mapping and schedule
-            const mapDistribute& distMap = mpp.map();
-
             const fvPatch& nbrPatch = refCast<const fvMesh>
             (
                 nbrMesh
@@ -165,7 +162,7 @@ Foam::scalarField Foam::radiationCoupledBase::emissivity() const
 
                 scalarField emissivity(temissivity);
                 // Use direct map mapping to exchange data
-                distMap.distribute(emissivity);
+                mpp.distribute(emissivity);
                 //Pout << emissivity << endl;
                 return emissivity;
             }
